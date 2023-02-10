1da3a2eb5fd8c90b9d1a60f41493a9a231f4963f
Alexandra <pinheiro.alexandra21@gmail.com>
2023-02-11 00:17:01 +0100

    footer css & social media icons

--- amahe/src/components/FooterLayout.vue
+++ amahe/src/components/FooterLayout.vue
@@ -1,10 +1,14 @@
 <script>
-import logo from "@/assets/fleur.svg";
+import logo from "@/assets/logoFooter.svg";
+import fb from "@/assets/fb.png";
+import inst from "@/assets/inst.png";
 
 export default {
   data() {
       return {
         logo: logo,
+        fb: fb,
+        inst:inst
         };
     },
 };
@@ -12,7 +16,7 @@ export default {
 
 <template>
   <footer id="footer">
-    <section class="">
+    <section class="footer-container">
         <div class="logo-container">
           <!--LOGO-->
           <a href ="#main" class="link-header">
@@ -21,12 +25,46 @@ export default {
               <span class="first-part">Aux mains </span>
               <span class="second-part">d'Amahé</span>
           </div>
-        </a>
+          </a>
+        </div>
+        <div class="social-media">
+          <img class="fb-icon" v-bind:src="fb">
+          <img class="inst-icon" v-bind:src="inst">
+        </div>
+        <div class="condition">
+
         </div>
     </section>
   </footer>
 </template>
 
 <style scoped>
+#footer {
+  height: 15em;
+  display: flex;
+  justify-content: center;
+  background-color: #FFFAF1;
+}
+a {
+  text-decoration: none;
+  color: #2F4858;
+}
 
+/*Smartphone Size*/
+@media only screen and (max-width: 680px) {
+  .footer-container {
+   
+  }
+  .social-media {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+  .logo {
+    width: 10em;
+  }
+  .fb-icon {
+    width: 1.9em;
+  }
+}
 </style>
