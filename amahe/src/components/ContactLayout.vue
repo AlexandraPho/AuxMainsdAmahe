ad5ce4b6c09e4cc892520b3589e5737bb27ed156
Alexandra <pinheiro.alexandra21@gmail.com>
2023-03-14 22:43:45 +0100

    change code for the contact card

--- amahe/src/components/ContactLayout.vue
+++ amahe/src/components/ContactLayout.vue
@@ -29,29 +29,30 @@ export default {
                 <div class="contact-infos">
                     <div class="item">
                         <img class="icon" alt="icon" v-bind:src="adress">
-                        <div class="text">
-                            <p> Quincy-Voisins, 77860</p>
-                            <p> Croissy-Beaubourg, 77183</p>
-                        </div>                          
+                        <address class="text">
+                            <p>Quincy-Voisins, 77860</p>
+                            <p>Croissy-Beaubourg, 77183</p>
+                        </address>                          
                     </div>
+                    <hr>
                     <div class="item">
                       <img class="sub-icon" alt="icon" v-bind:src="phone">
                         <div class="text">
                             <a href="tel:+33769832189">07 69 83 21 89</a>
                         </div>                          
                     </div>
+                    <hr>
                     <div class="item">
                       <img class="sub-icon" alt="icon" v-bind:src="mail">
                         <div class="text">
                           <a href="mailto:auxmainsdamahe@outlook.fr?subject=Mail envoyé du site" target="_blank">Envoyez-moi un mail</a>
                         </div>
                     </div>
+                    <hr>
                     <div class="item">
                       <img class="icon" alt="icon" v-bind:src="calendar">
                         <div class="text">
                           <table>
-                            <thead>
-                            </thead>
                             <tbody>
                               <tr>
                                 <td>Lundi </td>
@@ -77,8 +78,9 @@ export default {
                 </div>                  
             </div> 
     </section>
-    <div id="map" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20983.21825499983!2d2.8406314344982495!3d48.898199435308435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e8a6e446e559b9%3A0x40b82c3688c4ed0!2s77860%20Quincy-Voisins%2C%20France!5e0!3m2!1sfr!2sus!4v1678459827973!5m2!1sfr!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    </div>
+   <!-- <div id="map" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20983.21825499983!2d2.8406314344982495!3d48.898199435308435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e8a6e446e559b9%3A0x40b82c3688c4ed0!2s77860%20Quincy-Voisins%2C%20France!5e0!3m2!1sfr!2sus!4v1678459827973!5m2!1sfr!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
+    </div>-->
   </div>
 </template>
 
@@ -107,6 +109,10 @@ h3,
 th{
   color: #2F4858;
 }
+address {
+  font-style: normal;
+  text-align: start;
+}
 p { 
   margin-top: 0.5em;
 }
@@ -114,7 +120,6 @@ p {
   display: flex;
   flex-direction: column;
   border: 1px none;
-  padding: 1.8em;
   background-color: #eff8f0;
   border-radius: 1.5em;
   box-shadow: 5px 5px 15px #c9cbc9;
@@ -124,6 +129,16 @@ tr,
 p {
   color: #548086;
 }
+a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
+  }
+a:hover {
+    color: #006D61;
+    text-decoration: underline;
+  }
 td {
   text-align: center;
 }
@@ -131,7 +146,7 @@ td {
   padding-right: 1.5em;
 }
 table {
-  margin-top: 1.2em;
+  margin-top: 1.5em;
 }
 
 /*Smartphone Size*/
@@ -151,6 +166,7 @@ table {
   }
   .contact-infos {
     width: 20em;
+    padding: 1.5em;
   }
   .item {
     display: flex;
@@ -168,13 +184,51 @@ table {
     font-size: 2em;
     text-align: center;
     margin: 0.6em;
-    padding-top: 1.5em;
+    padding-top: 1.2em;
   }
-  .icon {
-    width: 2.5em;
+  .contact-card {
+    border-radius: 0.5rem;
+    border: 1px solid 1px;
+    padding: 0 30px 0 25px;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    max-width: 50em;
+    margin: auto 100px;
+    padding: 1.2em;
   }
-  .sub-icon {
-    width: 2em;
+  .contact-infos {
+    -webkit-align-self: center;
+        -ms-flex-item-align: center;
+            align-self: center;
+  }
+  .contact .contact-infos .item {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-wrap: wrap;
+        -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            margin: 0.8em;
+  }
+  .contact .contact-infos .item .icon,
+  .contact .contact-infos .item .sub-icon {
+    display: block;
+    font-size: 16px;
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    border: 1px solid #2F4858;
+    line-height: 34px;
+    text-align: center;
+    padding: 0.3em;
+    margin-right: 15px;
+  }
+  .item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   }
   p, td {
     font-size: 1em;
@@ -193,12 +247,54 @@ table {
     padding-top: 1.5em;
     font-size: 2.8em;
   }
+  .contact-card {
+    border-radius: 0.5rem;
+    border: 1px solid 1px;
+    padding: 0 30px 0 25px;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    max-width: 40em;
+    margin: 0 auto 100px;
+    padding: 1.5em;
+  }
+  .contact-infos {
+    -webkit-align-self: center;
+        -ms-flex-item-align: center;
+            align-self: center;
+  }
+  .contact .contact-infos .item {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-wrap: wrap;
+        -ms-flex-wrap: wrap;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            margin: 1em;
+  }
+  .contact .contact-infos .item .icon,
+  .contact .contact-infos .item .sub-icon {
+    display: block;
+    font-size: 16px;
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+    border: 1px solid #2F4858;
+    line-height: 34px;
+    text-align: center;
+    padding: 0.3em;
+    margin-right: 25px;
+  }
   .icon {
     width: 4em;
   }
-  .sub-icon {
-    width: 1.8em;
+  .item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   }
+
   th,
   tr {
     font-size: 1em;
@@ -208,11 +304,6 @@ table {
   p,
   td {
     font-size: 1.2em;
-  }
-  a {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 }
 
